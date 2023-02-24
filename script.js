f747a9b11f5b02d46eab4352e5882c2fc7bed29d
raghulvishnupv196476 <83441214+raghulvishnupv196476@users.noreply.github.com>
2023-02-24 19:34:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
+    Footer
+    © 2023 GitHub, Inc.
+    Footer navigation
+    Terms
+    Privacy
+    Secu
